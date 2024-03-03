a5bb707aa80de85a4db0b5183e2419321c90dd15
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2024-03-03 13:21:02 +0100

    Update course module to work with new django

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,4 @@
-from django.core.urlresolvers import reverse_lazy
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from braces.views import LoginRequiredMixin, PermissionRequiredMixin
