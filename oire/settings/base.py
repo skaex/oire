4b5064fbc6c6a2b95b5a4ae1a0e10a6391fe2390
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2024-03-03 13:32:44 +0100

    Use env instead

--- oire/settings/base.py
+++ oire/settings/base.py
@@ -86,11 +86,11 @@ WSGI_APPLICATION = 'oire.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'oire',
-        'USER': 'oire',
-        'PASSWORD': 'erio',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
